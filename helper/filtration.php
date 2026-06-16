50723f07f4c450110b68d0981272432e676d604d
RawanAhmed00 <ahmdrwan577@gmail.com>
2026-06-17 00:58:27 +0300

    feat: improve filtration function for appointment retrieval with enhanced query structure

--- helper/filtration.php
+++ helper/filtration.php
@@ -1,21 +1,37 @@
 <?php
-require __DIR__ ."/../config/database.php";
-require __DIR__ ."/../controllers/AppointmentController.php";
+require_once __DIR__ . "/../config/database.php";
+//require __DIR__ . "/../controllers/AppointmentController.php";
+require_once __DIR__. "/status.php";
 
 // Filtration 
+function GetFilter($conn) {
 
-function GetFilter ($conn){
+    $query = "SELECT * FROM appointments WHERE 1=1";
+    $bindings = [];
 
-$query ="select * from Appointments where 1=1";
-$bindings =[];
+    if (!empty($_GET["status"])) {
+       
+        $query .= " AND status = :status"; 
+        $bindings[":status"] = $_GET["status"];
+    }
 
-if (!empty ($_GET["status"])){
-    $query.="And status =:status";
-    $bindings[":status"] =$_GET["status"];
-}
+    if (!empty($_GET["date_time"])) {
+        $query .= " AND date_time = :date_time"; 
+        $bindings[":date_time"] = $_GET["date_time"];
+    }
 
-if (!empty ($_GET ["date_time"])){
-    $query.="And date_time =:date_time";
-    $bindings [":date_time"] =$_GET ["date_time"];
-}
-}
+    
+    $stmt = $conn->prepare($query);
+    
+    $stmt->execute($bindings);
+    
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+} 
+$res=GetFilter($conn);
+header("Content-Type: application/json");
+echo json_encode([
+    "status_code" => 200,
+    "message" => "Filtered appointments successfully",
+    "data" => $res
+]);
+exit;
