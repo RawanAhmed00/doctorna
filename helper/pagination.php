d6e824140bb5fb50d0390c4660b69aefdd2069de
RawanAhmed00 <ahmdrwan577@gmail.com>
2026-06-17 23:09:10 +0300

    docs: Improve documentation in pagination helper for clarity and completeness

--- helper/pagination.php
+++ helper/pagination.php
@@ -1,16 +1,42 @@
 <?php
 // helper/pagination.php
-//Pgintation concept: 
+/*Pagintation concept: 
+
+Display Number ($limit) of Elemnts in one page
+
+$Page -> current page number
+
+$limit -> number of elements in one page
+
+offset -> Start From What Elemnt? OR:How many records to skip before beginning?
+
+-offset->beginning = ($page -1 ) * $limit
+
+Array that has the allowed tables to determine tabes we want to engage
+
+if table name is not in allowed tables, display "invalid table name"
+
+To Display Number Of Pages: 
+$totalPages =
+ceil($totalRecords / $limit);
+ceil() -> used to round the number up to nearest integer
+
+Number Of Records ->Number Of Rows Of This Table
+
+*/
+
 function paginateTable($conn, $tableName, $defaultLimit = 10) {
     $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
     $limit = isset($_GET['limit']) ? (int)$_GET['limit'] : $defaultLimit;
     
+    //Secuirty: Page should not be less than one AND Limit should not be  less than one or more than 100
     if ($page < 1) $page = 1;
     if ($limit < 1 || $limit > 100) $limit = $defaultLimit;
 
+    //OFFSET: How many records to skip before beginning, it has a specefic equation
     $offset = ($page - 1) * $limit;
 
-    
+    //Array to specify Allowed tables:
     $allowedTables = ['speciality', 'doctors', 'users', 'appointments','sub_services']; 
     if (!in_array($tableName, $allowedTables)) {
         die(json_encode(["message" => "Invalid table name"]));
@@ -29,6 +55,7 @@ function paginateTable($conn, $tableName, $defaultLimit = 10) {
     $dataStmt->execute();
     $list = $dataStmt->fetchAll(PDO::FETCH_ASSOC);
 
+    //Total pages = totalrecords(rows we have in the table)/limit(no of rows in one page)
     $totalPages = ceil($totalRecords / $limit);
 
     return [
