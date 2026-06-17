a7912d4aa1ac58fec95eeaad204c085913825a73
RawanAhmed00 <ahmdrwan577@gmail.com>
2026-06-18 00:20:10 +0300

    fix: Correct typo in login comment for clarity

--- repos/AuthRepo.php
+++ repos/AuthRepo.php
@@ -4,7 +4,7 @@ require_once __DIR__ .'/../helper/db.php';
 
 // start with logging in(
 // 1-check if this email exists, if exists:1-check of the password written to password in database
-// if correct :tal3 message:Welcome!
+// if correct : message:Welcome!
 // if not correct message:password not correct
 // if email does not exist: user not found, please sign up
 // if yes: welcome to the system
